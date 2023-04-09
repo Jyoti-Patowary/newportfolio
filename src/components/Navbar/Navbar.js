df6250b0a94dcc287590a0d6351a32325c8550d4
Jyoti-Patowary <jpatowary8@gmail.com>
2023-04-09 11:35:28 +0530

    Initialize project using Create React App

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
