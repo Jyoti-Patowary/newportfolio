f08877fb3421e6a3ed1aaf82a157375d06092294
Jyoti-Patowary <jpatowary8@gmail.com>
2023-04-09 20:41:37 +0530

    portfolio section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography } from '@mui/material';
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <Container maxWidth="sm">
+      <Typography variant="h4" sx={{ mb: 1, fontStyle: "italic", color: 'red' }}>Testimonials</Typography>
+    </Container>
+  );
 }
 
-export default Contact
+export default Contact;
